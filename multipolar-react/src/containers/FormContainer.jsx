39a3bc2897d83b4147f941abd04f781510078240
qisti <qisti.rahmah@gmail.com>
2020-08-28 13:22:25 +0700

    add basic

---
+++ multipolar-react/src/containers/FormContainer.jsx
@@ -0,0 +1,172 @@
+import React, { Component } from "react";
+
+/* Import Components */
+import CheckBox from "../components/CheckBox";
+import Input from "../components/Input";
+import Select from "../components/Select";
+import Button from "../components/Button";
+
+class FormContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newUser: {
+        name: "",
+        age: "",
+        gender: "",
+        skills: [],
+        about: "",
+        agreement: ""
+      },
+      newData: {
+        nomorAtm: "",
+        tanggalLahir: null,
+        expDate: null,
+        digitPin: "",
+        captcha: ""
+      },
+
+      agreementOption: ["Saya setuju dengan syarat & ketentuan yang berlaku"],
+      genderOptions: ["Male", "Female", "Others"]
+    };
+    this.handleAge = this.handleAge.bind(this);
+    this.handleFullName = this.handleFullName.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleClearForm = this.handleClearForm.bind(this);
+    this.handleCheckBox = this.handleCheckBox.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  /* This lifecycle hook gets executed when the component mounts */
+
+  handleFullName(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          name: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleAge(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          age: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleInput(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          [name]: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleCheckBox(e) {
+    const newSelection = e.target.value;
+    let newSelectionArray;
+
+    if (this.state.newUser.skills.indexOf(newSelection) > -1) {
+      newSelectionArray = this.state.newUser.skills.filter(
+        s => s !== newSelection
+      );
+    } else {
+      newSelectionArray = [...this.state.newUser.skills, newSelection];
+    }
+
+    this.setState(prevState => ({
+      newUser: { ...prevState.newUser, skills: newSelectionArray }
+    }));
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    let userData = this.state.newUser;
+
+    fetch("http://example.com", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      response.json().then(data => {
+        console.log("Successful" + data);
+      });
+    });
+  }
+
+  handleClearForm(e) {
+    e.preventDefault();
+    this.setState({
+      newUser: {
+        name: "",
+        age: "",
+        gender: "",
+        skills: [],
+        about: ""
+      }
+    });
+  }
+
+  render() {
+    return (
+      <form className="container-fluid" onSubmit={this.handleFormSubmit}>
+        <Input
+          inputType={"text"}
+          title={"Nomor ATM"}
+          name={"nomorAtm"}
+          value={this.state.newData.nomorAtm}
+          handleChange={this.handleInput}
+        />{" "}
+        {/* Name of the user */}
+        <CheckBox
+          name={"agreement"}
+          options={this.state.agreementOption}
+          selectedOptions={this.state.newUser.skills}
+          handleChange={this.handleCheckBox}
+        />{" "}
+        {/* About you */}
+        <Button
+          action={this.handleFormSubmit}
+          type={"primary"}
+          title={"Lanjut"}
+          style={buttonStyle}
+        />{" "}
+        {/*Submit */}
+        <Button
+          action={this.handleClearForm}
+          type={"secondary"}
+          title={"Clear"}
+          style={buttonStyle}
+        />{" "}
+        {/* Clear the form */}
+      </form>
+    );
+  }
+}
+
+const buttonStyle = {
+  margin: "10px 10px 10px 10px"
+};
+
+export default FormContainer;
